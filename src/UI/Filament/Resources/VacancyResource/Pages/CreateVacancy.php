b67f1eee7ba8f155a0f5fedb4e393414a00c4e18
Anastas Mironov <ast@l80.ru>
2023-07-12 10:13:22 +0600

    fix: redirect to list page after create

--- src/UI/Filament/Resources/VacancyResource/Pages/CreateVacancy.php
+++ src/UI/Filament/Resources/VacancyResource/Pages/CreateVacancy.php
@@ -18,4 +18,9 @@ class CreateVacancy extends CreateRecord
             Actions\LocaleSwitcher::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return VacancyResource::getUrl();
+    }
 }
